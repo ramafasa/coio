96dfb9d112c347696f6f68c9a152ae185c6ee364
Rafal Maciak <rafal.maciak@harman.com>
2018-09-25 11:36:01 +0200

    Initial commit of COIO project

---
+++ src/main/java/com/rafit/coio/coio/CoioApplication.java
@@ -0,0 +1,12 @@
+package com.rafit.coio.coio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoioApplication.class, args);
+	}
+}
